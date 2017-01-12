2647f3ad8764f86eb6b2884ae28255ca1271de73
vdeleeuw <valerian.deleeuw@gmail.com>
2017-01-12 11:52:53 +0100

    correction direct input

--- src/main.cpp
+++ src/main.cpp
@@ -62,40 +62,38 @@ bool file_test_exists(string filename){
  */
 
 void parse_to_cpp(vector<char*> fic_ezl, string &input_files){
-    for(unsigned int i=0; i<fic_ezl.size(); ++i){
-        cout << "\033[1;36mFile parsing : \033[1;37m" << fic_ezl[i] << endl;
-        cout << "\033[1;36m=====================================\033[0m" << endl;
-        yyin = fopen(fic_ezl[i], "r");
+	if(!directinput){
+		for(unsigned int i=0; i<fic_ezl.size(); ++i){
+		    cout << "\033[1;36mFile parsing : \033[1;37m" << fic_ezl[i] << endl;
+		    cout << "\033[1;36m=====================================\033[0m" << endl;
+		    yyin = fopen(fic_ezl[i], "r");
 
-        if(!directinput){
+	        if(!yyin){
+	            cerr <<  fic_ezl[i] << ": file opening failed." << endl;
+	        }else{
+	            // creation des fichiers cpp
+	            string fichier_tmp = string(fic_ezl[i]);
 
-            if(!yyin){
-                cerr <<  fic_ezl[i] << ": file opening failed." << endl;
-            }
-            else{
-                // creation des fichiers cpp
-                string fichier_tmp = string(fic_ezl[i]);
-
-                fichier_tmp = fichier_tmp.substr(fichier_tmp.find_last_of("/")+1, fichier_tmp.find_last_of(".") - fichier_tmp.find_last_of("/"));
-                fichier_tmp +="cpp";
-                FILE * cpp_file = fopen(fichier_tmp.c_str(), "w");		
-				
+	            fichier_tmp = fichier_tmp.substr(fichier_tmp.find_last_of("/")+1, fichier_tmp.find_last_of(".") - fichier_tmp.find_last_of("/"));
+	            fichier_tmp +="cpp";
+	            FILE * cpp_file = fopen(fichier_tmp.c_str(), "w");		
+			
 				// cas où la création du fichier échoue
-                if(cpp_file == NULL){
-                    cerr << fichier_tmp << ": creation failed;" << endl;
-                    break;
-                }
+	            if(cpp_file == NULL){
+	                cerr << fichier_tmp << ": creation failed;" << endl;
+	                break;
+	            }
 
-                // parsing du fichiers ez en fichier cpp
-                yyparse();
+	            // parsing du fichiers ez en fichier cpp
+	            yyparse();
 
-                yyout = cpp_file;
+	            yyout = cpp_file;
 
-                // fermerture du fichier cpp
-                fclose(cpp_file);
-                input_files+=fichier_tmp + " ";
-            }
-        }
+	            // fermerture du fichier cpp
+	            fclose(cpp_file);
+	            input_files+=fichier_tmp + " ";
+	        }
+		}
     }
 }
 
@@ -108,7 +106,7 @@ void display(vector<char*> fic_ezl){
 
 
 void exec_cpp(std::string commande_gpp, std::string output_name){
-	cout << "commande cpp: " << commande_gpp << endl;
+	//cout << "commande cpp: " << commande_gpp << endl;
 	if(help != 1){
 		cout << commande_gpp << endl;
 		system(commande_gpp.c_str());
