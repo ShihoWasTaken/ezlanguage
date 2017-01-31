316175fa1a1b85d25e73e2b2a0cdd309db94e6a0
flodavid <fl.david.53@gmail.com>
2017-01-31 13:22:27 +0100

    déplacement des fichiers d'exemples et suppression des cpp

--- tests/other_tests.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-
-COMPILER=../bin/EZ_language_compiler
-
-# Execute tests
-echo "Test 1 : person" 
-$COMPILER person.ez
-echo "Test 2 : class" 
-$COMPILER test_class.ez
--- tests/unvalid_tests.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-
-COMPILER=../bin/EZ_language_compiler
-
-# Execute tests
-echo "Test 1 : julia" 
-$COMPILER julia.ez
-
